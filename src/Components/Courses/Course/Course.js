574006238c3458301a081cbae195ccdf5c940f43
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-08-27 03:23:40 +0600

    course cmponent added

---
+++ src/Components/Courses/Course/Course.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Course = (prpps) => {
+  return (
+    <div>
+      <div>this is a java course</div>
+    </div>
+  );
+};
+
+export default Course;
--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import courses from "../../FakeData/index";
-
+import course from "./Course/Course";
 const Courses = () => {
   const [allCourses, setallCourses] = useState(courses);
   return (
     <div>
       <div>courses will be added here</div>
       <div>{allCourses.length}</div>
+      <div></div>
     </div>
   );
 };
