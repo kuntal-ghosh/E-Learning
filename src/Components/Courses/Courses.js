88a1ceb75cbc071fc3a86a6e8cfed853ab65e844
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-08-27 03:27:41 +0600

    course component is added in courses component

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import courses from "../../FakeData/index";
-import course from "./Course/Course";
+import Course from "./Course/Course";
 const Courses = () => {
   const [allCourses, setallCourses] = useState(courses);
   return (
     <div>
       <div>courses will be added here</div>
       <div>{allCourses.length}</div>
-      <div></div>
+      <div>
+        {allCourses.map((course) => (
+          <Course course={course}></Course>
+        ))}
+      </div>
     </div>
   );
 };
